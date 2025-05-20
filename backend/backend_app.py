8d08e58b07ec1d64cd0efc6fd17852e2b596e212
Red.prjects <red.me1aqw@gmail.com>
2025-05-21 00:01:22 +0200

    Create the POST route.

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
+
 
 POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
@@ -13,6 +15,44 @@ POSTS = [
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+# # Attaches the flask app to the posts route.
+@app.route('/api/posts', methods=['POST'])
+def add_posts():
+    """
+    Creates a new post by loading first the list of POSTS, validates data for the title and content
+    , and initializes a new list to store values if there is no title or content, then shows an error
+    to the user.
+    :return: if there is no title or content, it returns an error to the user.
+    Otherwise, it returns the new post.
+    """
+    new_post = request.get_json()
+    # data validation
+    title = new_post.get('title')
+    content = new_post.get('content')
+    if not title or not content:
+        missing_fields = []
+        if not title:
+            missing_fields.append('title')
+        if not content:
+            missing_fields.append('content')
+        return jsonify({"error": f"Missing field(s): {', '.join(missing_fields )}"}), 400
+
+    # Generate new ID:
+    new_id = max((post['id'] for post in POSTS), default=0) + 1
+
+    # Create new post:
+    new_post = {
+        "id": new_id,
+        "title": title,
+        "content": content
+    }
+
+    POSTS.append(new_post)
+    print("The new post created successfully.")
+    return jsonify(new_post), 201
+
 
 
 if __name__ == '__main__':
